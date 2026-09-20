83303ebaa5aff3a59530d8088872da6ef77ffdd0
Isaiah Jassser Otilano <otilanoisaiahjasser@gmail.com>
2026-09-20 22:55:43 +0800

    update UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import ValidatorPage from './features/validator/ValidatorPage.jsx';
-
-import URLForm from "./ui/URLForm.jsx";
+import URLForm from './ui/URLForm.jsx';
 
 export default function App() {
-  return <ValidatorPage />;
-  return <URLForm />;
+  return (
+    <>
+      <ValidatorPage />
+      <URLForm />
+    </>
+  );
 }
